429c2238909563198652ee5c02c6f4cd619bc036
Dennis Siemensma <github@dennissiemensma.nl>
2017-01-21 18:26:57 +0100

    Renamed settings/constants to prevent misunderstandings.

--- dsmr_backend/tests/test_backend.py
+++ dsmr_backend/tests/test_backend.py
@@ -67,7 +67,7 @@ class TestBackend(InterceptStdoutMixin, TestCase):
     def test_supported_vendors(self):
         """ Check whether supported vendors is as expected. """
         self.assertEqual(
-            settings.DSMR_SUPPORTED_DB_VENDORS,
+            settings.DSMRREADER_SUPPORTED_DB_VENDORS,
             ('postgresql', 'mysql')
         )
 
